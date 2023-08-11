7725df91903eb17ac2cdb7b5b8d5a3599bc5f945
cezary17 <cezarykujath@gmail.com>
2023-08-11 12:59:37 +0200

    idk what to write here

--- src/cleaning.py
+++ src/cleaning.py
@@ -1,14 +1,29 @@
 import re
 
 
-def clean_block(block: str) -> str:
-    cleaned = [clean_line(line) for line in block]
-    for line in cleaned:
-        print(line)
+def clean_line(entry: str) -> str:
+    entry = re.sub("public.", "", entry)
+    entry = re.sub("true", "\"t\"", entry)
+    entry = re.sub("false", "\"f\"", entry)
+    entry = clean_constraints(entry)
+    return entry
 
 
-def clean_line(line: str) -> str:
-    line = re.sub("public.", "", line)
-    line = re.sub("true", "\"t\"", line)
-    line = re.sub("false", "\"f\"", line)
-    return line
+def clean_constraints(entry: str) -> str:
+
+    pattern = re.compile(r"CONSTRAINT")
+
+    constraint_found = pattern.search(entry)
+
+    if constraint_found:
+
+        lines = entry.split("\n")
+
+        lines = [line for line in lines if not pattern.search(line)]
+
+        # trim the leftover comma from last line of attribute definition
+        lines[-3] = lines[-3][:-1]
+
+        entry = "\n".join(lines)
+
+    return entry
--- src/main.py
+++ src/main.py
@@ -1,26 +1,73 @@
 import pgdumplib
+import os
+import sys
+import typing
+import argparse
 from pathlib import Path
 from cleaning import clean_line
 
 PATH_TO_DUMP = Path(Path().cwd().parent.parent, "backups", "dump_custom.sql")
 
-dump = pgdumplib.load(PATH_TO_DUMP)
+PATH_TO_OUTPUT = Path(Path().cwd().parent, "sqlite_dump.sql")
 
-tables = set()
+PATH_TO_TESTING = Path(Path().cwd(), "testing.sql")
 
-with open("schema.sql", "w") as schema_f, open("data.sql", "w") as data_f:
+PATH_TO_TESTING_OUTPUT = sys.stdout
 
-    for entry in dump.entries:
-        if entry.desc == "TABLE":
-            line = clean_line(entry.defn)
-            table_name = line.split(" ")[2]
-            tables.add(table_name)
 
-            schema_f.write(line)
+def parse_dump(path_to_dump: os.PathLike, path_to_output: os.PathLike) -> typing.NoReturn:
 
-    for table in tables:
-        for line in dump.table_data("public", table):
-            data_f.write(clean_line(line[0]))
-            data_f.write("\n")
+    tables = set()
 
-        data_f.write("\n")
+    print("Starting to parse the dump file")
+
+    dump = pgdumplib.load(path_to_dump)
+
+    with open(path_to_output, "w") as file:
+
+        file.write("BEGIN;\n")
+
+        for entry in dump.entries:
+            if entry.desc == "TABLE":
+                line = clean_line(entry.defn)
+                table_name = line.split(" ")[2]
+                tables.add(table_name)
+
+                file.write(line)
+
+        tables.remove("external_data")  # Problems with parsing, useless anyways
+
+        for table in tables:
+            for line in dump.table_data("public", table):
+                file.write(clean_line(line[0]))
+                file.write("\n")
+
+            file.write("\n")
+
+        file.write("END;\n")
+
+    print("Parsing finished")
+
+
+def test_cleaning(path_to_dump: os.PathLike) -> typing.NoReturn:
+
+    with open(path_to_dump, "r") as infile:
+        entry = infile.read()
+
+    print(clean_line(entry))
+
+
+def main():
+    parser = argparse.ArgumentParser(description="True and Test run split")
+    parser.add_argument("-t", "--test", action="store_true", help="Set if you want to test run")
+
+    args = parser.parse_args()
+
+    if args.test:
+        test_cleaning(PATH_TO_TESTING)
+    else:
+        parse_dump(PATH_TO_DUMP, PATH_TO_OUTPUT)
+
+
+if __name__ == "__main__":
+    main()
