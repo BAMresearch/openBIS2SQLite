8cbc0b899fc41d4e34afd28b665f4696bfb92cea
cezary17 <cezarykujath@gmail.com>
2023-09-11 09:23:25 +0200

    moving from gitlab

---
+++ src/classes.py
@@ -0,0 +1,164 @@
+import re
+import typing
+from dataclasses import dataclass
+
+import sqlparse
+from pgdumplib.dump import Entry
+
+from cleaning import (
+    clean_change_parenthesis,
+    clean_line,
+    clean_defaults_body,
+    parse_insert_attributes,
+    parse_insert_values_ast,
+    remove_apostrophes_phone_number,
+    split_body_table,
+    split_hb_table,
+    split_insert,
+)
+from constants import BruhMoment, EntryType, NewlineInEntryError
+
+
+@dataclass
+class DumpBase:
+    pgdumplib_entry: Entry
+
+    def __init__(self, entry: Entry):
+        self.pgdumplib_entry = entry
+        self.__entry_type: EntryType = EntryType[entry.desc]
+        self.__tag: str = entry.tag
+
+    @property
+    def entry_type(self) -> EntryType:
+        return self.__entry_type
+
+    @property
+    def tag(self) -> str:
+        return self.__tag
+
+
+@dataclass
+class DumpTable(DumpBase):
+    pgdumplib_entry: Entry
+    header: str
+    body: typing.List[str]
+    constraint_flag: bool
+    attributes: typing.List[str]
+
+    def __init__(self, entry: Entry):
+
+        sql_entry = entry.defn
+
+        constraint_pat = re.compile(r"CONSTRAINT")
+        self.constraint_flag = constraint_pat.search(sql_entry)
+
+        sql_entry = clean_line(sql_entry)
+
+        self.header, temp_body = split_hb_table(sql_entry)
+        temp_body = split_body_table(temp_body)
+        self.body = clean_defaults_body(temp_body)
+
+        super(DumpTable, self).__init__(entry=entry)
+
+    def __hash__(self):
+        return hash(self.__entry_type, self.__tag)
+
+    def __str__(self) -> str:
+
+        combined_body = ",\n".join([line.rjust(len(line) + 4) for line in self.body])
+
+        return f"{self.header} (\n{combined_body}\n);\n"
+
+    def get_attributes(self) -> typing.List[str]:
+        attributes = []
+
+        for line in self.body:
+            attribute = line.split(" ")[0]
+            attributes.append(attribute)
+
+        return attributes
+
+    def apply_mappings(self, rules: typing.Dict[str, str]) -> typing.NoReturn:
+
+        pattern = re.compile(r".*varying\([0-9]+\)")
+        self.body = [line.split(" ") for line in self.body]
+
+        for line in self.body:
+            if pattern.match(" ".join(line)):
+                line.pop(2)
+            try:
+                line[1] = rules[line[1]]
+            except KeyError:
+                line[1] = "TEXT"
+
+        self.body = [" ".join(line) for line in self.body]
+
+
+@dataclass
+class DumpInsert():
+    pgdumplib_entry: Entry
+    header: str
+    attributes: typing.List[str]
+    values: typing.List[str]
+    table: str
+    sqlparse_obj: sqlparse.sql.Statement
+
+    def __init__(self, entry: str, table_attrs: typing.List[str]):
+
+        sql_entry = clean_line(entry)
+
+        if not sql_entry.endswith(");"):
+            # print(sql_entry)
+            raise NewlineInEntryError
+
+        self.header, temp_attributes, temp_values = split_insert(sql_entry)
+        self.attributes = parse_insert_attributes(temp_attributes)
+        temp_values = remove_apostrophes_phone_number(temp_values)
+        temp_values = clean_change_parenthesis(temp_values)
+        try:
+            self.values = parse_insert_values_ast(temp_values)
+        except SyntaxError as err:
+            print("\n" * 5)
+            print(sql_entry)
+            raise SyntaxError(err)
+        self.table = self.header.split(" ")[2]
+
+        if not len(self.attributes) == len(self.values):
+            if len(self.attributes) > len(self.values):
+                self.attributes = self.attributes[:len(self.values)]
+            else:
+                self.values = self.values[:len(self.attributes)]
+
+        to_remove = set(self.attributes) ^ set(table_attrs)
+
+        assert to_remove < set(self.attributes)
+
+        for to_remove_attr in list(to_remove):
+            self.pop_attribute(to_remove_attr)
+
+        if not len(self.attributes) == len(self.values):
+            # problem = commas in string values break parsing
+            print(f"attributes lenght: {len(self.attributes)}")
+            print(f"values lenght: {len(self.values)}")
+            for idx in range(min(len(self.attributes), len(self.values))):
+                print(f"{self.attributes[idx]}: {self.values[idx]}")
+            if len(self.attributes) < len(self.values):
+                print(self.values[-1])
+            else:
+                print(self.attributes[-1])
+            raise BruhMoment
+
+    def __str__(self) -> str:
+
+        combined_attributes = ", ".join(self.attributes)
+        combined_values = [f"\'{element}\'" if isinstance(element, str) else str(element) for element in self.values]
+        combined_values = ", ".join(combined_values)
+        combined_values = re.sub("None", "NULL", combined_values)
+
+        return f"{self.header} ({combined_attributes}) VALUES ({combined_values});\n"
+
+    def pop_attribute(self, attribute_name: str) -> str:
+        attribute_index = self.attributes.index(attribute_name)
+
+        self.attributes.pop(attribute_index)
+        return self.values.pop(attribute_index)
---
+++ src/cleaning.py
@@ -0,0 +1,208 @@
+import re
+import typing
+from ast import literal_eval
+
+import sqlparse
+
+from constants import CONVERTING_RULES, BruhMoment
+
+
+def clean_line(entry: str) -> str:
+    entry = re.sub(r"public\.", "", entry)
+    entry = re.sub("true", "True", entry)
+    entry = re.sub("false", "False", entry)
+    entry = clean_constraints(entry)
+    entry = clean_trim_after_pattern(entry, "with", "DOMAIN")
+    entry = clean_trim_after_pattern(entry, "DEFAULT", r".*(DOMAIN).*")
+    return entry
+
+
+def clean_constraints(entry: str) -> str:
+
+    constraint_pat = re.compile(r"CONSTRAINT")
+    domain_pat = re.compile(r"DOMAIN")
+
+    constraint_found = constraint_pat.search(entry)
+
+    if constraint_found:
+
+        lines = entry.split("\n")
+
+        lines = [line for line in lines if not constraint_pat.search(line)]
+
+        is_domain = domain_pat.search(entry)
+
+        if is_domain:
+            # add semicolon to end of line
+            lines[-2] = f"{lines[-2]};"
+        else:
+            # trim the leftover comma from last line of attribute definition
+            lines[-3] = lines[-3][:-1]
+
+        entry = "\n".join(lines)
+
+    return entry
+
+
+def clean_trim_after_pattern(entry: str, pattern: str, check: str) -> str:
+
+    search_pat = re.compile(pattern)
+    domain_pat = re.compile(check)
+
+    if not (search_pat.search(entry) and domain_pat.search(entry)):
+        return entry
+
+    entry_split = entry.split(pattern, 1)[0].rstrip()
+    return f"{entry_split};\n"
+
+
+def clean_defaults_body(body: typing.List[str]) -> typing.List[str]:
+    return [line.split(" DEFAULT", 1)[0] for line in body]
+
+
+def get_data_type_mapping(entry: str, clean: bool = True) -> typing.Tuple[str, str]:
+
+    if clean:
+        entry = clean_line(entry)
+
+    key_part, val_part = entry.split(" AS ")
+    key = key_part.split(" ")[2]
+    val_unconverted = re.sub(r";\n", "", val_part)
+
+    try:
+        val = CONVERTING_RULES[val_unconverted]
+    except KeyError:
+        val = "TEXT"  # changed from blob
+
+    return key, val
+
+
+def add_defaults_to_rules(rules: typing.Dict[str, str]) -> typing.Dict[str, str]:
+    defaults = {
+        "integer": "INTEGER",
+        "character": "TEXT",
+        "real": "REAL",
+        "boolean": "INTEGER",
+        "bigint": "INTEGER",
+        "smallint": "INTEGER"
+    }
+
+    return rules | defaults
+
+
+def apply_mappings(entry: str, rules: typing.Dict[str, str]) -> str:
+
+    header, body = entry.split("(", 1)
+
+    body = body.split(",")
+    body = [line.strip().removesuffix("\n);").split(" ") for line in body]
+
+    pattern = re.compile(r".*varying\([0-9]+\)")
+
+    for line in body:
+        if pattern.match(" ".join(line)):
+            line.pop(2)
+        try:
+            line[1] = rules[line[1]]
+        except KeyError:
+            line[1] = "TEXT"
+
+    body = [" ".join(line) for line in body]
+    body = [line.rjust(len(line) + 4) for line in body]
+    body = ",\n".join(body)
+
+    entry = f"{header}(\n{body}\n);\n"
+
+    return entry
+
+
+def split_hb_table(entry: str) -> typing.Tuple[str, str]:
+    header, body = entry.split("(", 1)
+    header = header.strip()
+    body = body.strip().removesuffix("\n);")
+
+    return header, body
+
+
+def split_body_table(body: str) -> typing.List[str]:
+    body_split = body.split(",")
+    body_split = [line.strip() for line in body_split]
+
+    return body_split
+
+
+def split_insert(entry: str) -> typing.Tuple[str, str, str]:
+    entry_split = entry.split(" ")
+
+    values_index = entry_split.index("VALUES")
+
+    header = " ".join(entry_split[0:3])
+    attributes = " ".join(entry_split[3:values_index])
+    values = " ".join(entry_split[values_index + 1:])
+
+    return header, attributes, values
+
+
+def parse_insert_attributes(attributes: str) -> typing.List[str]:
+    trimmed_attributes = attributes.lstrip("(").rstrip(")")
+    split_attributes = trimmed_attributes.split(",")
+    split_attributes = [attribute.strip() for attribute in split_attributes]
+    return split_attributes
+
+
+def parse_insert_values(values: str) -> typing.List[str]:
+    trimmed_values = values.lstrip("(").rstrip(");")
+    split_values = trimmed_values.split(",")
+    split_values = [value.strip() for value in split_values]
+    return split_values
+
+
+def parse_insert_values_sqlparse(statement: sqlparse.sql.Statement):
+
+    for field in statement.tokens:
+        if isinstance(field, sqlparse.sql.Values):
+            values_field = field
+            break
+    else:
+        print("ERROR LOG : PARSE INSERT VALUES")
+        print("\n")
+        print(statement.tokens)
+        print("\n")
+        for field in statement.tokens:
+            print(field)
+            print("\n")
+        raise BruhMoment
+
+    for values_token in values_field:
+        if isinstance(values_token, sqlparse.sql.Parenthesis):
+            values_generator = values_token._groupable_tokens[0].get_identifiers()
+            break
+
+    return [str(token) for token in values_generator]
+
+
+def parse_insert_values_ast(values: str) -> typing.List[str]:
+    trimmed_values = values.rstrip(";")
+    trimmed_values = re.sub("NULL", "None", trimmed_values)
+    return list(literal_eval(trimmed_values))
+
+
+def clean_change_parenthesis(entry: str) -> str:
+    pat = re.compile(r"'(\"(.+)\":(\w|,)+\s?)*((''([\w/\-\.\+@]+)'')|('''))(:(\w|,)+)\s?('(.*)':[\w,]+\s?)*'")  # noqa: E501
+    guard = 0
+
+    # TODO: Here the result contains a space at the end of the string
+    while pat.search(entry) and guard < 50:
+        entry = pat.sub("'\\1\"\\6\"\\8 \\10'", entry)
+        guard += 1
+
+    return entry
+
+
+def remove_apostrophes_phone_number(entry: str) -> str:
+    pat = re.compile(r"'([\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6})'''")
+
+    while pat.search(entry):
+        entry = pat.sub("'\\1\"'", entry)
+
+    return entry
