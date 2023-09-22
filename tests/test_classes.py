6f465c4874cecf260624ec46deb778f32bf6c8ac
cezary17 <cezarykujath@gmail.com>
2023-09-22 12:47:23 +0200

    other path issues

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,5 +1,5 @@
 import pytest
-from openbis2sqlite.classes import DumpTable, DumpInsert
+from classes import DumpTable, DumpInsert
 from enum import Enum
 from pathlib import Path
 from pgdumplib.dump import Entry
